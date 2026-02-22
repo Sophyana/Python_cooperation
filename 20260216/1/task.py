79ddccd6437fa7d667171f18ae87ee1b15b4274c
Sophyana <sophya_esaulenko@mail.ru>
2026-02-22 22:33:12 +0300

    display last commit object of a branch

--- 20260216/1/task.py
+++ 20260216/1/task.py
@@ -1,5 +1,16 @@
 from pathlib import Path
 import sys
+import zlib
+
+
+def find_commit(repo_path, commit_hash):
+    with open(f"{repo_path}/.git/objects/{commit_hash[:2]}/{commit_hash[2:]}", "rb") as f:
+        message = f.read()
+
+    decompressed_message = zlib.decompress(message)
+    obj_type, _, content = decompressed_message.partition(b"\x00")
+
+    return content.decode()
 
 
 def one_arg(repo_path):
@@ -13,6 +24,14 @@ def one_arg(repo_path):
         print(item.name)
 
 
+def two_arg(repo_path, branch_name):
+    with open(f"{repo_path}/.git/refs/heads/{branch_name}", "r") as f:
+        commit = f.read()
+
+    content = find_commit(repo_path, commit[:-1])
+    print(content)
+
+
 if len(sys.argv) < 2:
     print("Использование: script.py <путь_к_каталогу> [<имя_ветки>]")
     sys.exit(1)
@@ -21,3 +40,6 @@ repo_path = sys.argv[1]
 
 if len(sys.argv) == 2:
     one_arg(repo_path)
+else:
+    branch_name = sys.argv[2]
+    two_arg(repo_path, branch_name)
