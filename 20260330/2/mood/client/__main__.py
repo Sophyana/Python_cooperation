67bba69a9f6204425c01fc66b7909716bcf6d1d0
Sophyana <sophya_esaulenko@mail.ru>
2026-04-05 14:00:46 +0300

    mud: modularize as mood package with flake8 and pydocstyle

--- 20260330/2/mood/client/__main__.py
+++ 20260330/2/mood/client/__main__.py
@@ -1,35 +1,13 @@
-from io import StringIO
-from cowsay import read_dot_cow, list_cows
+"""Модуль клиента с проверкой аргументов команд и их дополнением."""
+from cowsay import list_cows
 import shlex
 import sys
-import cmd
-import readline
 import asyncio
 from prompt_toolkit.history import InMemoryHistory
 from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit import PromptSession
-
-
-class Game(cmd.Cmd):
-    prompt = ">> "
-    intro = "<<< Welcome to Python-MUD 0.1 >>>"
-    field_size = 10
-    custom_monsters = {
-        "jgsbat": read_dot_cow(StringIO(r"""
-        $the_cow = <<EOC;
-            ,_                    _,
-            ) '-._  ,_    _,  _.-' (
-            )  _.-'.|\\\\--//|.'-._  (
-             )'   .'\/o\/o\/'.   `(
-              ) .' . \====/ . '. (
-               )  / <<    >> \  (
-                '-._/``  ``\_.-'
-          jgs     __\\\\'--'//__
-                 (((""`  `"")))
-        EOC
-        """))
-    }
+from ..common import HOST, PORT, FIELD_SIZE, INTRO, CUSTOM_MONSTERS
 
 
 history = InMemoryHistory()
@@ -38,14 +16,11 @@ commands = ["addmon", "attack", "up", "down", "left", "right",
 
 
 class DynamicCompleter(Completer):
-    def __init__(self):
-        self.commands_args = {
-            "addmon": ["hello", "hp", "coords"]
-        }
+    """Класс дополнения команд."""
 
     def get_completions(self, document, complete_event):
+        """Дополнение аргументов команд."""
         text = document.text_before_cursor
-
 
         if not text:
             for cmd in commands:
@@ -60,11 +35,11 @@ class DynamicCompleter(Completer):
         match words:
 
             case["addmon"]:
-                for monster in list_cows() | Game.custom_monsters.keys():
-                        yield Completion(monster, start_position=0)
+                for monster in list_cows() | CUSTOM_MONSTERS.keys():
+                    yield Completion(monster, start_position=0)
 
             case ["attack"]:
-                for monster in list_cows() | Game.custom_monsters.keys():
+                for monster in list_cows() | CUSTOM_MONSTERS.keys():
                     yield Completion(monster, start_position=0)
 
             case[first]:
@@ -73,7 +48,7 @@ class DynamicCompleter(Completer):
                         yield Completion(cmd, start_position=-len(first))
 
             case ["attack", name_monster]:
-                for monster in list_cows() | Game.custom_monsters.keys():
+                for monster in list_cows() | CUSTOM_MONSTERS.keys():
                     if monster.startswith(name_monster):
                         if name_monster == monster:
                             yield Completion("with", start_position=0)
@@ -92,8 +67,9 @@ class DynamicCompleter(Completer):
                 for w in ["sword", "axe", "bow"]:
                     if w.startswith(weapon) and w != weapon:
                         yield Completion(w, start_position=-len(weapon))
+
             case ["addmon", name]:
-                for monster in list_cows() | Game.custom_monsters.keys():
+                for monster in list_cows() | CUSTOM_MONSTERS.keys():
                     if monster.startswith(name):
                         if name != monster:
                             yield Completion(monster, start_position=-len(name))
@@ -104,12 +80,18 @@ class DynamicCompleter(Completer):
                 yield from self.complete_addmon_arguments(text)
 
     def complete_addmon_arguments(self, text):
+        """
+        Дополнение аргументов команды addmon.
+
+        Реализована отдельно, потому что её аргументы могут стоять в разной последовательности.
+        """
+        addmon_args = ["hello", "hp", "coords"]
         words = shlex.split(text)
         provided_args = set(words[1:])
         last_word = words[-1] if words else ""
-        for arg in self.commands_args["addmon"]:
+        for arg in addmon_args:
             if arg not in provided_args:
-                if text.endswith((" ", "\t")) and last_word not in self.commands_args["addmon"]:
+                if text.endswith((" ", "\t")) and last_word not in addmon_args:
                     yield Completion(arg, start_position=0)
                 if arg.startswith(last_word):
                     yield Completion(arg, start_position=-len(last_word))
@@ -119,7 +101,10 @@ completer = DynamicCompleter()
 
 
 class Client:
-    def __init__(self, username, host='localhost', port=8888):
+    """Класс клиента."""
+
+    def __init__(self, username, host=HOST, port=PORT):
+        """Инициализация клиента."""
         self.host = host
         self.port = port
         self.username = username
@@ -128,10 +113,12 @@ class Client:
         self.session = PromptSession(history=self.history, completer=self.completer)
 
     async def run(self):
+        """Запуск клиента."""
         await self.connect()
         await asyncio.gather(self.receive_messages(), self.send_messages())
 
     async def connect(self):
+        """Подключение к серверу."""
         self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
         self.writer.write(self.username.encode() + b"\n")
         await self.writer.drain()
@@ -142,21 +129,21 @@ class Client:
             print("Player with this nickname already exists")
             sys.exit(1)
         else:
-            print("<<< Welcome to Python-MUD 0.1 >>>")
+            print(INTRO)
 
     async def send_messages(self):
+        """Отправка сообщений."""
         loop = asyncio.get_running_loop()
         try:
             with patch_stdout():
                 while True:
                     command = await loop.run_in_executor(None,
-                                lambda: self.session.prompt(">> "))
+                                                         lambda: self.session.prompt(">> "))
                     if not command:
                         continue
 
                     test_command = shlex.split(command)
                     namecommand = test_command[0]
-                    field_size = 10
 
                     if namecommand not in commands:
                         print("Unknown command")
@@ -182,7 +169,7 @@ class Client:
                             continue
 
                         name = args[0]
-                        if name not in list_cows() | Game.custom_monsters.keys():
+                        if name not in list_cows() | CUSTOM_MONSTERS.keys():
                             print(f"Unknown monster {name}")
                             continue
 
@@ -210,8 +197,8 @@ class Client:
                                 continue
 
                             x, y = params['coords']
-                            if not (0 <= x < field_size and 0 <= y < field_size):
-                                print(f"Invalid coordinates\nField size is {field_size}x{field_size}")
+                            if not (0 <= x < FIELD_SIZE and 0 <= y < FIELD_SIZE):
+                                print(f"Invalid coordinates\nField size is {FIELD_SIZE}x{FIELD_SIZE}")
                                 continue
                         except (ValueError, IndexError):
                             print(f"Invalid arguments\n{usage}")
@@ -238,6 +225,7 @@ class Client:
             print("Disconnected.")
 
     async def receive_messages(self):
+        """Простое отображение сообщений от сервера."""
         while True:
             try:
                 msg = await self.reader.readline()
@@ -251,7 +239,7 @@ class Client:
 if __name__ == '__main__':
 
     if len(sys.argv) != 2:
-        print("Usage: python client.py <username>")
+        print("Usage: python -m mood.client <username>")
         sys.exit(1)
 
     username = sys.argv[1]
--- 20260330/2/mood/server/__main__.py
+++ 20260330/2/mood/server/__main__.py
@@ -1,36 +1,24 @@
-import sys
-from io import StringIO
-from cowsay import read_dot_cow, cowthink
+"""Сервер Python-MUD."""
+from cowsay import cowthink
 import shlex
 import cmd
-import readline
 import asyncio
-
+from ..common import HOST, PORT, FIELD_SIZE, CUSTOM_MONSTERS
 
 monsters = {}
 users = dict()
 
+
 class Game(cmd.Cmd):
-    prompt = ">> "
-    intro = "<<< Welcome to Python-MUD 0.1 >>>"
-    field_size = 10
-    custom_monsters = {
-        "jgsbat": read_dot_cow(StringIO(r"""
-        $the_cow = <<EOC;
-            ,_                    _,
-            ) '-._  ,_    _,  _.-' (
-            )  _.-'.|\\\\--//|.'-._  (
-             )'   .'\/o\/o\/'.   `(
-              ) .' . \====/ . '. (
-               )  / <<    >> \  (
-                '-._/``  ``\_.-'
-          jgs     __\\\\'--'//__
-                 (((""`  `"")))
-        EOC
-        """))
-    }
+    """
+    Основной класс для выполнения команд игры.
+
+    Этот класс отвечает за обработку команд пользователя
+    и управление логикой игры.
+    """
 
     def __init__(self, writer, nickname):
+        """Инициализация игры."""
         super().__init__()
         self.player_x = 0
         self.player_y = 0
@@ -44,54 +32,72 @@ class Game(cmd.Cmd):
         self.nickname = nickname
 
     def say_all(self, msg):
+        """
+        Отобразить сообщение абсолютно всем.
+
+        Нужно для сообщений об убийстве моба и его создании.
+        """
         for client in users.values():
             client.write(msg)
 
     def do_sayall(self, message):
+        """
+        Отобразить сообщение всем, кроме пишущего.
+
+        Нужно для чатика.
+        """
         for client in users.values():
             if client != self.writer:
                 client.write(f"{self.nickname}: {shlex.split(message)[0]}\n".encode())
 
     def do_exit(self, arg):
+        """Выход из игры."""
         self.writer.write(b"exit...\n")
         return b"exit\n"
 
     def do_status(self, arg):
+        """Отобразить статус сервера для отладки."""
         self.writer.write(b"Server work.\n")
 
     def encounter(self, x, y):
+        """Энкаунтер при встрече с монстром."""
         if (x, y) in monsters:
             name, hello, hp = monsters[(x, y)]
-            if name in self.custom_monsters:
-                self.writer.write((cowthink(hello, cowfile=self.custom_monsters[name])+"\n").encode())
+            if name in CUSTOM_MONSTERS:
+                self.writer.write((cowthink(hello, cowfile=CUSTOM_MONSTERS[name]) + "\n").encode())
             else:
-                self.writer.write((cowthink(hello, cow=name)+"\n").encode())
+                self.writer.write((cowthink(hello, cow=name) + "\n").encode())
 
     def move_player(self, dx, dy, arg):
-        """Generalized player movement function"""
-        self.player_x = (self.player_x + dx) % self.field_size
-        self.player_y = (self.player_y + dy) % self.field_size
+        """
+        Общая функция передвижения игрока по полю.
+
+        Она получает только изменения координат dx, dy от команд
+        up, down, left, right.
+        """
+        self.player_x = (self.player_x + dx) % FIELD_SIZE
+        self.player_y = (self.player_y + dy) % FIELD_SIZE
         self.writer.write(f"Moved to ({self.player_x}, {self.player_y})\n".encode())
         self.encounter(self.player_x, self.player_y)
 
     def do_up(self, arg):
-        """Move the player up."""
+        """Движение вверх."""
         self.move_player(0, -1, arg)
 
     def do_down(self, arg):
-        """Move the player down."""
+        """Движение вниз."""
         self.move_player(0, 1, arg)
 
     def do_left(self, arg):
-        """Move the player left."""
+        """Движение влево."""
         self.move_player(-1, 0, arg)
 
     def do_right(self, arg):
-        """Move the player right."""
+        """Движение вправо."""
         self.move_player(1, 0, arg)
 
     def do_addmon(self, arg):
-        "Add a monster to the game."
+        """Добавить монстра на поле."""
         args = shlex.split(arg)
         name = args[0]
         params = {}
@@ -116,12 +122,11 @@ class Game(cmd.Cmd):
         monsters[(x, y)] = (name, hello, hp)
         self.say_all(f"{self.nickname} add a monster {name} at ({x}, {y}) saying '{hello}' with {hp} HP\n".encode())
         if replaced:
-            self.say_all(f"{self.nickname} replaced the old monster in ({x} {y}) to a monster {name} at ({x}, {y}) saying '{hello}' with {hp} HP \n".encode())
-
+            self.say_all(f"{self.nickname} replaced the old monster in ({x} {y}) to a monster {name}"
+                         f" at ({x}, {y}) saying '{hello}' with {hp} HP \n".encode())
 
     def do_attack(self, arg):
-        """Attack the monster in the current cell."""
-
+        """Атаковать монстра с выбором оружия."""
         args = shlex.split(arg)
         name_monster = args[0]
         self.player_weapon = args[2]
@@ -132,7 +137,7 @@ class Game(cmd.Cmd):
             self.writer.write(b"No monster here\n")
             return
 
-        if name_monster not in monsters[(x,y)]:
+        if name_monster not in monsters[(x, y)]:
             self.writer.write(f"No {name_monster} here\n".encode())
             return
 
@@ -150,12 +155,16 @@ class Game(cmd.Cmd):
 
 
 class Server:
-    def __init__(self, host='localhost', port=8888):
+    """Сервер."""
+
+    def __init__(self, host=HOST, port=PORT):
+        """Инициализация сервера на заданном порте и хосте."""
         self.host = host
         self.port = port
         self.clients = dict()
 
     async def handle_client(self, reader, writer):
+        """При подключении клиента, проверяем ник в системе и запускаем играть."""
         addr = writer.get_extra_info('peername')
         username = (await reader.readline()).decode().strip()
 
@@ -192,6 +201,7 @@ class Server:
             await writer.wait_closed()
 
     async def run(self):
+        """Запуск сервера."""
         server = await asyncio.start_server(self.handle_client, self.host, self.port)
         addr = ', '.join(str(sock.getsockname()) for sock in server.sockets)
         print(f'Working on {addr}')
